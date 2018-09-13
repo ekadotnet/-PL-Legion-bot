f7f077567e09fd349b09e454db3e7d6bdc79bb94
Unknown <jankos.pdp@gmail.com>
2018-09-14 01:49:31 +0200

    Added help option to existing commands

--- commands/say.js
+++ commands/say.js
@@ -1,12 +1,28 @@
 execute = async (message, args) => {
-  const say = args.join(" ");
-  message.delete().catch(O_o => {});
-  if (say === "" || say === null) {
-    await message.channel.send(
-      "Co mam się kurwa domyśleć co powiedzieć? Daj jakiś tekścior ziomek"
-    );
+  if (args[0] === "help") {
+    let user = message.author.id;
+
+    await message.channel.send(`<@${user}>`, {
+      embed: {
+        title: `!say`,
+        description: `Command will make bot say whatever you want`,
+        fields: [
+          { name: `Usage:`, value: `!say [text]` },
+          { name: `Optional parameters:`, value: `none` },
+          { name: `Examples`, value: `!say Kiana best girl` }
+        ]
+      }
+    });
   } else {
-    await message.channel.send(say);
+    const say = args.join(" ");
+    message.delete().catch(O_o => {});
+    if (say === "" || say === null) {
+      await message.channel.send(
+        "Co mam się kurwa domyśleć co powiedzieć? Daj jakiś tekścior ziomek"
+      );
+    } else {
+      await message.channel.send(say);
+    }
   }
 };
 
--- commands/utils.js
+++ commands/utils.js
@@ -1,17 +1,53 @@
-ping = async message => {
-  const m = await message.channel.send("Ping?");
-  m.edit(
-    `Pong! Latency is ${m.createdTimestamp -
-      message.createdTimestamp}ms. Latency is ${Math.round(client.ping)}ms`
-  );
+ping = async (message, args) => {
+  if (args[0] === "help") {
+    let user = message.author.id;
+
+    await message.channel.send(`<@${user}>`, {
+      embed: {
+        title: `!ping`,
+        description: `Command will measue user's ping`,
+        fields: [
+          { name: `Usage:`, value: `!ping` },
+          { name: `Optional parameters:`, value: `none` },
+          { name: `Examples`, value: `!ping` }
+        ]
+      }
+    });
+  } else {
+    const m = await message.channel.send("Ping?");
+    m.edit(
+      `Pong! Latency is ${m.createdTimestamp -
+        message.createdTimestamp}ms. Latency is ${Math.round(client.ping)}ms`
+    );
+  }
 };
 
 me = async (message, args, user) => {
-  const activity = args.join(" ");
-  if (activity === "" || activity === null) {
-    await message.channel.send(`${user} is too dumb to pass me arguments :(`);
+  if (args[0] === "help") {
+    let user = message.author.id;
+
+    await message.channel.send(`<@${user}>`, {
+      embed: {
+        title: `!me`,
+        description: `Displays user's current activity or the one given by user`,
+        fields: [
+          { name: `Usage:`, value: `!me [activity]` },
+          { name: `Optional parameters:`, value: `none` },
+          { name: `Examples`, value: `!me watching anime` }
+        ]
+      }
+    });
   } else {
-    await message.channel.send(`${user} *is ${activity}*`);
+    const activity = args.join(" ");
+    let username = message.author.username;
+
+    if (activity === "" || activity === null) {
+      await message.channel.send(
+        `${username} is too dumb to pass me arguments :(`
+      );
+    } else {
+      await message.channel.send(`${username} *is ${activity}*`);
+    }
   }
 };
 
