bb111e65824901875aae8e5c36131028b4080490
Jan Dziedziela <jdziedziela@pgs-soft.com>
2019-02-04 13:48:46 +0100

    extracted constants to fields, refactored sending messages in boop.js

--- commands/boop.js
+++ commands/boop.js
@@ -1,15 +1,18 @@
-const helper = require("../shared/helper.js");
+const sender = require("../shared/sender.js");
 
-execute = async (message, args) => {
+const boopDescription = {
+  title: `!boop`,
+  description: `This command only usage is to annoy Boop >:)`
+};
+
+const boop = "188416390330318848";
+
+const execute = async (message, args) => {
   if (args[0] === "help") {
-    const helpData = {
-      title: `!boop`,
-      description: `This command only usage is to annoy Boop >:)`
-    };
-    await helper.getHelp(message.channel, message.author.id, helpData);
+    await sender.getHelp(message.channel, message.author.id, boopDescription);
   } else {
-    const boop = "188416390330318848";
-    await message.channel.send(`<@${boop}> >:(`);
+    let message = `<@${boop}> >:(`;
+    await sender.sendMessage(message.channel, message);
   }
 };
 
