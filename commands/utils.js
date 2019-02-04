968f6fb905bf14a37f8ab1b608b9b68f65aa2b31
Jan Dziedziela <jdziedziela@pgs-soft.com>
2019-02-04 13:51:27 +0100

    utils: moved descriptions to const fields, fixed awaits, refactored sending messages

--- commands/utils.js
+++ commands/utils.js
@@ -1,30 +1,62 @@
-const helper = require("../shared/helper.js");
+const sender = require("../shared/sender.js");
 
-ping = async (message, args, client) => {
+const pingDescription = {
+  title: `!ping`,
+  description: `Command will measure user's ping`,
+  fields: [
+    {
+      name: `Usage:`,
+      value: `!ping`
+    },
+    {
+      name: `Optional Parameters:`,
+      value: `none`
+    },
+    {
+      name: `Examples:`,
+      value: `!ping`
+    }
+  ]
+};
+
+const meDescription = {
+  title: `!me`,
+  description: `Displays activity given by user`,
+  fields: [
+    {
+      name: `Usage:`,
+      value: `!me [activity]`
+    },
+    {
+      name: `Optional Parameters:`,
+      value: `none`
+    },
+    {
+      name: `Examples:`,
+      value: `!me watching anime`
+    }
+  ]
+};
+
+const helpDescription = {
+  title: `Who am I?`,
+  description: `Simple, lewd oriented bot for [PL]Legion armada`,
+  fields: [
+    {
+      name: `Available commands:`,
+      value: `**!ping !me !say !boop !r !momo !danbooru !commands !help**\nTo get more specific info:\n**!command help** (for example: **!r help**)`
+    },
+    { name: `Author:`, value: `EnjoyTheNoise#2702` }
+  ]
+};
+
+const ping = async (message, args, client) => {
   let user = message.author.id;
 
   if (args[0] === "help") {
-    const helpData = {
-      title: `!ping`,
-      description: `Command will measure user's ping`,
-      fields: [
-        {
-          name: `Usage:`,
-          value: `!ping`
-        },
-        {
-          name: `Optional Parameters:`,
-          value: `none`
-        },
-        {
-          name: `Examples:`,
-          value: `!ping`
-        }
-      ]
-    };
-    await helper.getHelp(message.channel, user, helpData);
+    await sender.getHelp(message.channel, user, pingDescription);
   } else {
-    const m = await message.channel.send("Ping?");
+    const m = await sender.sendMessage(message.channel, "Ping?");
     m.edit(
       `<@${user}>\nPong!\nYour Latency: ${m.createdTimestamp -
         message.createdTimestamp}ms.\nAPI Latency: ${Math.round(client.ping)}ms`
@@ -32,96 +64,30 @@ ping = async (message, args, client) => {
   }
 };
 
-me = async (message, args) => {
-  let user = message.author.id;
-
+const me = async (message, args) => {
   if (args[0] === "help") {
-    const helpData = {
-      title: `!me`,
-      description: `Displays activity given by user`,
-      fields: [
-        {
-          name: `Usage:`,
-          value: `!me [activity]`
-        },
-        {
-          name: `Optional Parameters:`,
-          value: `none`
-        },
-        {
-          name: `Examples:`,
-          value: `!me watching anime`
-        }
-      ]
-    };
-    await helper.getHelp(message.channel, user, helpData);
+    await sender.getHelp(message.channel, message.author.id, meDescription);
   } else {
-    const activity = args.join(" ");
-    let username = message.author.username;
+    let activity = args.join(" ");
 
     if (activity === "" || activity === null) {
-      await message.channel.send(
-        `${username} is too dumb to pass me arguments :(`
-      );
+      let message = `${
+        message.author.username
+      } is too dumb to pass me arguments :(`;
+      await sender.sendMessage(message.channel, message);
     } else {
-      await message.channel.send(`${username} *is ${activity}*`);
-    }
-  }
-};
-
-say = async (message, args) => {
-  let user = message.author.id;
-
-  if (args[0] === "help") {
-    const helpData = {
-      title: `!say`,
-      description: `Command will make bot say whatever you want`,
-      fields: [
-        {
-          name: `Usage:`,
-          value: `!say [text]`
-        },
-        {
-          name: `Optional Parameters:`,
-          value: `none`
-        },
-        {
-          name: `Examples:`,
-          value: `!say Kiana best girl`
-        }
-      ]
-    };
-    await helper.getHelp(message.channel, user, helpData);
-  } else {
-    const say = args.join(" ");
-    message.delete().catch(error => console.log(error));
-    if (say === "" || say === null) {
-      await message.channel.send(`What am I supposed to say? >:(`);
-    } else {
-      await message.channel.send(say);
+      let message = `${message.author.username} *is ${activity}*`;
+      await sender.sendMessage(message.channel, message);
     }
   }
 };
 
 help = async message => {
-  let user = message.author.id;
-  const helpData = {
-    title: `Who am I?`,
-    description: `Simple, lewd oriented bot for [PL]Legion armada`,
-    fields: [
-      {
-        name: `Available commands:`,
-        value: `**!ping !me !say !boop !r !momo !danbooru !commands !help**\nTo get more specific info:\n**!command help** (for example: **!r help**)`
-      },
-      { name: `Author:`, value: `EnjoyTheNoise#2702` }
-    ]
-  };
-  await helper.getHelp(message.channel, user, helpData);
+  await sender.getHelp(message.channel, message.author.id, helpDescription);
 };
 
 module.exports = {
   ping: ping,
   me: me,
-  say: say,
   help: help
 };
