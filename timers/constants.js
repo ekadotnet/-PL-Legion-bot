52797d5570d6d7966b0e2bf5c0783bda9dc117ad
Unknown <jankos.pdp@gmail.com>
2019-02-07 23:50:49 +0100

    Moved consts to external file

---
+++ timers/constants.js
@@ -0,0 +1,26 @@
+module.exports = {
+  commandType: {
+    START: "start",
+    STOP: "stop"
+  },
+
+  daysOfWeek: {
+    MONDAY: 1,
+    TUESDAY: 2,
+    WEDNESDAY: 3,
+    THURSDAY: 4,
+    FRIDAY: 5,
+    SATURDAY: 6,
+    SUNDAY: 0
+  },
+
+  ABYSS_OPEN_TIME: 15,
+  ABYSS_CLOSE_TIME: 22,
+  ABYSS_CALC_OFFSET: 33,
+
+  OPEN_WORLD_CLOSE_TIME: 3,
+  OPEN_WORLD_RESET_TIME: 4,
+  OPEN_WORLD_LOCK_TIME: 60,
+
+  REFRESH_RATE: 30000
+};
