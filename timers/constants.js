cac4b2dacf9391f016c92103d86d0e3372ed9d1b
Unknown <jankos.pdp@gmail.com>
2019-02-10 16:33:18 +0100

    Update timers constants

--- timers/constants.js
+++ timers/constants.js
@@ -1,7 +1,31 @@
 module.exports = {
+  timersDescription: {
+    title: `\`timers`,
+    description: `Timers for abyss and open world`,
+    fields: [
+      {
+        name: `Usage:`,
+        value: `\`timers start`
+      },
+      {
+        name: `Optional parameters:`,
+        value: `none`
+      },
+      {
+        name: `Examples:`,
+        value: `timers start`
+      },
+      {
+        name: `**Important:**`,
+        value: `User can subscribe to timer by executing command **\`timers subscribe**`
+      }
+    ]
+  },
   commandType: {
     START: "start",
-    STOP: "stop"
+    STOP: "stop",
+    SUBSCRIBE: "subscribe",
+    HELP: "help"
   },
 
   daysOfWeek: {
@@ -17,10 +41,14 @@ module.exports = {
   ABYSS_OPEN_TIME: 15,
   ABYSS_CLOSE_TIME: 22,
   ABYSS_CALC_OFFSET: 33,
+  ABYSS_REMIND_OFFSET: 1,
 
   OPEN_WORLD_CLOSE_TIME: 3,
   OPEN_WORLD_RESET_TIME: 4,
   OPEN_WORLD_LOCK_TIME: 60,
 
-  REFRESH_RATE: 30000
+  REFRESH_RATE: 30000,
+
+  SUBSCRIBER_ROLE: "Remind me",
+  SUBSCRIBER_MSG: "abyss is ending!"
 };
